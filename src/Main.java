beb84effdcc1b11f5a2bbbe20efbff1c39c86f02
StanislavTi89 <Timoshenko311989@gmail.com>
2023-01-25 22:48:33 +0300

    ООП_Инкапсуляция

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ООП_Инкапсуляция");
         Car car = new Car("Лада", "Седан", 1.6, "Баклажан", 2005,
-                "Россия", "РУчная_Коробка", "Седан", "х123йу_RUS123",
+                "Россия", "РУчная_Коробка", "Седан", "х123йу_RUS23",
                 4, true, null);
         System.out.println(car);
 
