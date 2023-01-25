59be089067dddc646acc8361678db115874f83b6
StanislavTi89 <Timoshenko311989@gmail.com>
2023-01-25 22:34:49 +0300

    ООП_Инкапсуляция

--- src/transport/Car.java
+++ src/transport/Car.java
@@ -14,6 +14,32 @@ public class Car {
     private String regNumber;
     private final int capacity;
     private boolean summerTyres;
+    private Key key;
+
+    public static class Key {
+
+        private final boolean remoteRun;
+        private final boolean withoutAccess;
+
+        public Key(boolean remoteRun, boolean withoutAccess) {
+            this.remoteRun = remoteRun;
+            this.withoutAccess = withoutAccess;
+        }
+
+        public boolean isRemoteRun() {
+            return remoteRun;
+        }
+
+        public boolean isWithoutAccess() {
+            return withoutAccess;
+        }
+
+        @Override
+        public String toString() {
+            return (remoteRun ? "удаленный Старт" : "без удаленного Старта") + ", " + (withoutAccess ? "удаленный ключ доступ" : "удаленный ключ доступ отсутствует");
+        }
+
+    }
 
     public Car(String brand,
                String model,
@@ -25,7 +51,8 @@ public class Car {
                String typeOfBody,
                String regNumber,
                int capacity,
-               boolean summerTyres) {
+               boolean summerTyres,
+               Key key) {
         if (brand == null || brand.isEmpty()) {
             brand = "default";
         }
@@ -55,68 +82,57 @@ public class Car {
         }
         this.capacity = capacity;
         this.summerTyres = summerTyres;
+        setKey(key);
     }
 
     public String getBrand() {
         return brand;
     }
-
     public String getModel() {
         return model;
     }
-
     public double getEngineVolume() {
         return engineVolume;
     }
-
     public void setEngineVolume(double engineVolume) {
         if (engineVolume <= 0) {
             engineVolume = 1.5;
         }
         this.engineVolume = engineVolume;
     }
-
     public String getColor() {
         return color;
     }
-
     public void setColor(String color) {
         if (color == null || color.isEmpty()) {
             color = "белый";
         }
         this.color = color;
     }
-
     public int getYear() {
         return year;
     }
-
     public String getCountry() {
         return country;
     }
-
     public String getGear() {
         return gear;
     }
-
     public void setGear(String gear) {
         if (gear == null || gear.isEmpty()) {
             gear = "МКПП";
         }
         this.gear = gear;
     }
-
     public String getTypeOfBody() {
         return typeOfBody;
     }
-
     public String getRegNumber() {
         return regNumber;
     }
-
     public void setRegNumber(String regNumber) {
         if (regNumber == null || regNumber.isEmpty()) {
-            regNumber = "xx___xxx";
+            regNumber = "x000xx000";
         }
         this.regNumber = regNumber;
     }
@@ -132,7 +148,15 @@ public class Car {
     public void setSummerTyres(boolean summerTyres) {
         this.summerTyres = summerTyres;
     }
-
+    public Key getKey() {
+        return key;
+    }
+    public void setKey(Key key) {
+        if (key == null) {
+            key = new Key(false, false);
+        }
+        this.key = key;
+    }
     public void changeTyres(int month) {
         if ((month >= 11 && month <= 12) || (month >= 1 && month <= 3)) {
             summerTyres = false;
@@ -141,7 +165,6 @@ public class Car {
             summerTyres = true;
         }
     }
-
     public String toString() {
         return "Марка: " + brand + ", " + "модель: " + model + ", " + "объём двигателя в литрах: " + engineVolume + ", " +
                 "цвет кузова: " + color + ", " + "год производства: " + year + ", " + "страна сборки: " + country + ", " +
